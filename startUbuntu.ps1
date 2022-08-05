8e230ae595686258bfd2fa9c9d1ff2f6a2c18dcd
Kourtnee <kourtnee@pop-os.localdomain>
2022-08-04 20:00:04 -0400

    updated windows script

--- startUbuntu.ps1
+++ startUbuntu.ps1
@@ -1,8 +1,8 @@
 # Has to be authorized using:
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 $REPO="kourtnee/"
-$IMAGE="1001_vnc"
-$TAG="20.04"
+$IMAGE="cse1001-novnc"
+$TAG="latest"
 $RESOL="1440x900"
 docker run -d -p 6080:80 -v "$(PWD):/workspace:rw" -e "RESOLUTION=${RESOL}" --name "${IMAGE}-run" "${REPO}${IMAGE}:${TAG}"
 Start-Sleep -s 5
